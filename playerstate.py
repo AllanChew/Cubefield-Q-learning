079273e71b4d9ce84d529c7596aade781d5889ac
AllanChew <awchew@uwaterloo.ca>
2020-10-14 00:59:56 -0400

    Removed collision when turning around a corner

--- playerstate.py
+++ playerstate.py
@@ -29,15 +29,13 @@ class PlayerState:
             self.player_y_row = (gameref.generation_y_index + self.player_y_row_start - 1) % gameref.y_row_blocks # move to forward block
         else:
             self.player_y_row = old_player_y_row
-        
-        if moved_left_right: # will move left/right
-            if gameref.block_array[self.player_x_col][old_player_y_row] == 1: # check block immediately left/right
-                return -1 # hit left/right
-        if moved_front_block: # will move to forward block
-            if gameref.block_array[old_player_x_col][self.player_y_row] == 1: # check block in front
-                return -1 # hit front
-        if moved_left_right and moved_front_block: # will move diagonal
-            if gameref.block_array[self.player_x_col][self.player_y_row] == 1: # check new location
-                return -1 # hit diagonal
+
+        if moved_left_right or moved_front_block: # will move to new block
+            if gameref.block_array[self.player_x_col][self.player_y_row] == 1: # check new block
+                return -1 # new location is a solid block
+        if ( moved_left_right and moved_front_block and # will move diagonal
+             gameref.block_array[self.player_x_col][old_player_y_row] == 1 and # check block immediately left/right
+             gameref.block_array[old_player_x_col][self.player_y_row] == 1 ): # check block in front
+            return -1 # hit due to squeezing between two solid block corners
         return 0
 
